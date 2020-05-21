fbc7e9b035163627cbcb16d24ecee9e2c1a85258
nikkhilvijay07 <65727569+nikkhilvijay07@users.noreply.github.com>
2020-05-21 22:35:35 +0530

    Simple solution of most frequently asked question
    Here is the most simplest possible solution of this problem. 
    The wording of the problem is confuses. But logically if we think the sum of any given integer list would be minimum if start decreasing the maximum numbers. 
    
    The example tries to confuse as well. We we read it carefully, we understand the way to solve it .

--- minimumSum.py
+++ minimumSum.py
@@ -1,1 +1,31 @@
+'''
+Given an array of integers, perform some number k of operations. Each operation consists of removing
+any element from the array, dividing it by 2 and inserting the ceiling of that result back into the array.
+Minimize the sum of the elements in the final array.
+Example:
+nums = [10, 20, 7]
+k = 4
+Pick    Pick/2 Ceiling Result
+Initial array [10, 20, 7]
+7         3.5      4          [ 10, 20, 4]
+10         5       5           [5, 20, 4]
+20         10      10          [5, 10, 4]
+10         5        5          [5, 5, 4]
+The sum of the final array is 5 + 5 + 4 = 14, and that sum is minimal.
+'''
+
+
+
 import math
+
+def minimumSum(nums,k):
+  max_index = len(nums) - 1
+  for i in range(k):
+    #find index of maximun number 
+    max_index = nums.index(max(nums))
+    nums[max_index] = math.ceil(max(nums)/2) # repalce max_value by its half. 
+  return sum(nums)
+
+nums = [10,20,7]
+k= 4
+print(minimumSum(nums,k))
